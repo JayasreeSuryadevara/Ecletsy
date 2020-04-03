92c7983f9220d28e547450197512cb689ec2e340
JayasreeSuryadevara <sjayas@hotmail.com>
2020-04-03 15:32:24 -0700

    merging shopping cart debugging

--- frontend/components/products/product_index.jsx
+++ frontend/components/products/product_index.jsx
@@ -1,38 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-class ProductIndex extends React.Component {
-    constructor(props){
-        super(props);
-    }
-
-    render(){
-        const products = Object.values(this.props.products);
-        console.log("props in product-index", products);
-        const productList = products.map(product => {
-            return(
-                <li className='product-index-item' key={`product-${product.id}`}>
-                    <Link to={`/products/${product.id}`}>
-                        <div className='product-index-image'>
-                            <img src={product.image_url} />
-                        </div>
-                        <div className="product-index-item-info">
-                            <div className="product-index-price">${product.price.toFixed(2)}</div>
-                            <div className="free-shipping-bar">FREE Shipping</div>
-                        </div>
-                    </Link>
-                </li>
-            );
-        });
-    
+function ProductIndex() {
+    const products = Object.values(this.props.products);
+    const productList = products.map(product => {
         return(
-            <div className="product-index-container">
-                <ul className="product-index-ul">
-                    { productList }
-                </ul>
-            </div>
+            <li className='product-index-item' key={`product-${product.id}`}>
+                <Link to={`/products/${product.id}`}>
+                    <div className='product-index-image'>
+                        <img src={product.image_url} />
+                    </div>
+                    <div className="product-index-item-info">
+                        <div className="product-index-price">${product.price.toFixed(2)}</div>
+                        <div className="free-shipping-bar">FREE Shipping</div>
+                    </div>
+                </Link>
+            </li>
         );
-    }
+    });
+
+    return(
+        <div className="product-index-container">
+            <ul className="product-index-ul">
+                { productList }
+            </ul>
+        </div>
+    );
 };
 
 export default ProductIndex;
